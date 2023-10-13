04fdb7159f1b213454542c0d43abfc30f5951f7c
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-10-13 13:38:45 +0530

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'ropa': "'Ropa Sans', sans-serif",
+        'sofia': "'Sofia Sans Condensed', sans-serif;",
+      }
+    },
   },
   plugins: [],
 }
